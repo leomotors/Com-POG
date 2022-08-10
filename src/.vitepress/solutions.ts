67da7e6ad4096b69e9d7ad4a66a69ff4ada58a23
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2022-08-10 22:01:18 +0700

    demo: add 05_List_11

--- src/.vitepress/solutions.ts
+++ src/.vitepress/solutions.ts
@@ -17,4 +17,8 @@ export const solutions: DefaultTheme.SidebarItem[] = [
     text: "00_Intro_03 - ★ Mountain Valley",
     link: "/solutions/00_Intro_03",
   },
+  {
+    text: "05_List_11 - ★ Missing Digits",
+    link: "/solutions/05_List_11",
+  },
 ];
