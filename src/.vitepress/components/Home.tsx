148365bcfc43548bc2d6dc04603752823252b05c
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2022-08-16 18:21:57 +0700

    feat(home): use VPTeamMembers component instead

--- src/.vitepress/components/Home.tsx
+++ src/.vitepress/components/Home.tsx
@@ -1,40 +1,33 @@
 import { FunctionalComponent as FC } from "vue";
 
+import { VPTeamMembers } from "vitepress/theme";
+
 interface HomeProps {
-  contributors: Array<{ name?: string; login: string; avatarUrl: string }>;
+  contributors: Array<{ name?: string; login: string }>;
 }
 
 const Home: FC<HomeProps> = ({ contributors }) => {
   return (
-    <div>
-      <p class="mt-12 text-center text-4xl font-bold">🙏Contributors🙇</p>
+    <div class="px-4">
+      <p class="mt-12 mb-8 text-center text-4xl font-bold sm:text-5xl">
+        🙏Contributors🙇
+      </p>
 
-      <div class="mx-auto my-6 flex w-fit flex-col items-start gap-4">
-        {contributors.map((contributor) => (
-          <div class="flex flex-row gap-2">
-            <img
-              src={contributor.avatarUrl}
-              class="rounded-full"
-              width="50"
-              height="50"
-            />
-
-            <div>
-              <a
-                class="text-lg font-bold transition-colors hover:text-gray-600 dark:hover:text-gray-300"
-                href={"https://github.com/" + contributor.login}
-                target="_blank"
-                rel="noreferrer"
-              >
-                {contributor.name ?? contributor.login}
-              </a>
-              <p class="text-gray-700 dark:text-gray-200">
-                {contributor.login}
-              </p>
-            </div>
-          </div>
-        ))}
-      </div>
+      {/* @ts-expect-error */}
+      <VPTeamMembers
+        size="small"
+        members={contributors.map((contributor) => ({
+          avatar: `https://github.com/${contributor.login}.png`,
+          name: contributor.name ?? contributor.login,
+          title: contributor.login,
+          links: [
+            {
+              icon: "github",
+              url: `https://github.com/${contributor.login}`,
+            },
+          ],
+        }))}
+      />
     </div>
   );
 };
